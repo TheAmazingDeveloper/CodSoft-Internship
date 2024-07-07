a5e87977aeed516b4fecafe13d3b849aabea26aa
Darshit Jain <darshitjain09092006@gmail.com>
2024-07-07 15:48:50 +0530

    Security Fixes
    Swapped .innerHTML with .innerText for security purposes.

--- Level-1-Task-2-Calculator/index.js
+++ Level-1-Task-2-Calculator/index.js
@@ -2,38 +2,38 @@ let resultString = ''
 let result = 0
 
 const resultRef = document.querySelector("#result")
-resultRef.innerHTML = result
+resultRef.innerText = result
 
 const setResult = (newResult) => {
-    resultRef.innerHTML = newResult
+    resultRef.innerText = newResult
 }
 
 const btns = document.querySelectorAll("#btn")
 
 btns.forEach((btn) => {
     btn.addEventListener('click', (event) => {
-        let innerHTML = event.target.innerHTML
-        if (innerHTML == 'AC') {
+        let innerText = event.target.innerText
+        if (innerText == 'AC') {
             result = 0
             resultString = ''
             setResult(result)
-        } else if (innerHTML == '=') {
+        } else if (innerText == '=') {
             resultString = eval(resultString)
             setResult(resultString)
         } else {
-            if (innerHTML == 'X') {
+            if (innerText == 'X') {
                 resultString += '*'
                 setResult(resultString)
-            } else if (innerHTML == '÷') {
+            } else if (innerText == '÷') {
                 resultString += '/'
                 setResult(resultString)
-            } else if (innerHTML == '⌫') {
+            } else if (innerText == '⌫') {
                 resultString = resultString.replace(resultString.charAt(resultString.length - 1), '')
                 resultString.length == 0 ? setResult(0) : setResult(resultString)
-            } else if (innerHTML == '.') {
-            } else if (innerHTML == '( )') {
+            } else if (innerText == '.') {
+            } else if (innerText == '( )') {
             } else {
-                resultString += innerHTML
+                resultString += innerText
                 setResult(resultString)
             }
         }
